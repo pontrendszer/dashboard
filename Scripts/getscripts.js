ff3938532639dd3043f7aa342c43fdde61fa1b7b
devhelperrbx <smartwavescafe@gmail.com>
2023-07-31 15:39:18 +0200

    aaaa

--- Scripts/getscripts.js
+++ Scripts/getscripts.js
@@ -7,9 +7,9 @@ function handleButtonClick() {
     // Define the mapping of values to alerts
     const alertMessages = {
         '1. játék - Szókitaláló': 'Az első játék nem más mint (dobpergés)... név. Lényege. Hogyan fog zajlani. A sorrend: Balról az első talál a mellette lévő jobbra mond. Utána húz a mondó szavat, majd felrakjuk a találóra a fülest és indítjuk a zenét. Ha kitalálja 2 percen bellül, +1 pont, ha nem akkor nem kap pontot. Ez egy saját pontozású játék.',
-        '2. játék - Bingó': 'B',
-        '3. játék - Murder Mystery': 'C',
-        'Nyeremény kiosztás': 'WINNER'
+        '2. játék - Bingó': 'ERROR: NINCS MEGADVA SZÖVEG',
+        '3. játék - Murder Mystery': 'ERROR: NINCS MEGADVA SZÖVEG',
+        'Nyeremény kiosztás': 'ERROR: NINCS MEGADVA SZÖVEG'
     };
 
     const alertTitles = {
