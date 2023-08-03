c270c05e2f9354242e3fceb2974ed0fd1f9e7101
devhelperrbx <smartwavescafe@gmail.com>
2023-08-03 14:37:10 +0200

    Kaméleon

--- Scripts/getscripts.js
+++ Scripts/getscripts.js
@@ -8,14 +8,14 @@ function handleButtonClick() {
     const alertMessages = {
         '1. játék - Szókitaláló': 'Az első játék nem más mint (dobpergés)... név. Lényege. Hogyan fog zajlani. A sorrend: Balról az első talál a mellette lévő jobbra mond. Utána húz a mondó szavat, majd felrakjuk a találóra a fülest és indítjuk a zenét. Ha kitalálja 2 percen bellül, +1 pont, ha nem akkor nem kap pontot. Ez egy saját pontozású játék.',
         '2. játék - Bingó': 'A második játék a (dobpergés)... név. Szelvényel kiosztása. Tollak kiosztása. Lényege. Hogyan fog zajlani. 5 kör lesz. Ez egy közepes nehézségű játék. A körökből pontokat lehet szerezni, majd azokból az 1., 2., 3. kap listás pontot.',
-        '3. játék - Murder Mystery': 'ERROR: NINCS MEGADVA SZÖVEG',
+        '3. játék - Kaméleon': 'Az utolsó játék nem más mint a (dobpergés).. név. A lényege h folytasd a szót az előző szó utolsó betűjével! Ami volt, az nem lehet újra! ',
         'Nyeremény kiosztás': 'ERROR: NINCS MEGADVA SZÖVEG'
     };
 
     const alertTitles = {
         '1. játék - Szókitaláló': 'Szókitaláló',
         '2. játék - Bingó': 'Bingó',
-        '3. játék - Murder Mystery': 'Murder Mystery',
+        '3. játék - Kaméleon': 'Kaméleon',
         'Nyeremény kiosztás': 'Nyeremény kiosztás'
     };
 
